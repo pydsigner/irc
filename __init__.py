52fb59467bc1e20663b52c7a8138d9292e245dff
Daniel Foerster <pydsigner@gmail.com>
2012-01-30 15:22:12 -0600

    Python Package
    * Added the required __init__.py and moved shared docstring to it

---
+++ __init__.py
@@ -0,0 +1,20 @@
+'''
+IRC: A general purpose IRC interface for Python
+
+IRC is based heavily on Alan Bunbury's Bunbot, available freely from GitHub 
+(https://github.com/bunburya/bunbot). The code was fetched on 26 November 2011,
+and may now be different. This code may, at the author's discretion, be rebased
+on a newer version of Bunbot; this will be duly noted.
+
+The most significant changes:
+
+* Random coding style changes
+* Generalization of bot.Bot()
+* A few docstring additions
+* Cross-version Python compatibility
+
+--------------------------------------------------------------------------------
+
+(c) 2012 by Daniel Foerster <pydsigner@gmail.com> and redistributable under the 
+LGPL.
+'''
