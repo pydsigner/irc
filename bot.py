ddae23430688d7f1ec87526a83d49fafe98b8987
Daniel Foerster <pydsigner@gmail.com>
2012-01-30 12:20:32 -0600

    First commit

---
+++ bot.py
@@ -0,0 +1,84 @@
+'''
+IRC: A general purpose IRC interface for Python
+
+IRC is based heavily on Alan Bunbury's Bunbot, available freely from GitHub 
+(https://github.com/bunburya/bunbot). The code was fetched on 26 November 2011,
+and may now be different. This code may, at the author's discretion, be rebased
+on a newer version of Bunbot; this will be duly noted.
+
+The most significant changes:
+
+* Random coding style changes
+* Generalization of bot.Bot()
+* A few docstring additions
+* Cross-version Python compatibility
+
+--------------------------------------------------------------------------------
+
+2012 by Daniel Foerster <pydsigner@gmail.com> and redistributable under the 
+LGPL.
+
+--------------------------------------------------------------------------------
+
+This is a base for an IRC bot.
+'''
+
+from imp import reload
+
+import connect
+
+class Bot:
+    '''
+    An IRC bot interface.
+    '''
+    def __init__(self, config):
+        '''
+        @config must be a module with a CommandLib() class that presents a 
+        particular interface.
+        TODO: Describe the CommandLib() interface
+        '''
+        self.config = config
+        self.ident = config.Identity()
+        self.conn = connect.IRCConn(self)
+        self.cmds = config.CommandLib(self)
+        self.conn.connect()
+        self.conn.mainloop()
+    
+    def reload_cmds(self):
+        self.cmds = reload(self.config).CommandLib(self)
+    
+    def handle_privmsg(self, tokens, sender):
+        chan = tokens.pop(0)
+        tokens[0] = tokens[0].strip(':')
+        if tokens[0] == self.ident.nick:
+            tokens.pop(0)
+            is_to_me = True
+        else:
+            is_to_me = False
+
+        nick, host = sender.split('!')        
+        if chan == self.ident.nick:
+            chan = nick
+        data = {'channel': chan, 'sender': sender, 'is_to_me': is_to_me,
+                'nick': nick, 'host': host}
+        
+        cmd = tokens[0]
+        try:
+            args = tokens[1:]
+        except IndexError:
+            args = []
+        
+        if is_to_me and (cmd in self.cmds.addr_funcs):
+            self.cmds.addr_funcs[cmd](args, data)
+        elif cmd in self.cmds.unaddr_funcs:
+            self.cmds.unaddr_funcs[cmd](args, data)
+        else:
+            for func in self.cmds.all_privmsg_funcs:
+                func(tokens, data)
+        
+    def handle_other_join(self, tokens, joiner):
+        chan = tokens.pop()
+        nick, host = joiner.split('!')
+        data = {'channel': chan, 'joiner': joiner, 'nick': nick, 'host': host}
+        for func in self.cmds.other_join_funcs:
+            func(data)
---
+++ connect.py
@@ -0,0 +1,156 @@
+'''
+IRC: A general purpose IRC interface for Python
+
+IRC is based heavily on Alan Bunbury's Bunbot, available freely from GitHub 
+(https://github.com/bunburya/bunbot). The code was fetched on 26 November 2011,
+and may now be different. This code may, at the author's discretion, be rebased
+on a newer version of Bunbot; this will be duly noted.
+
+The most significant changes:
+
+* Random coding style changes
+* Generalization of bot.Bot()
+* A few docstring additions
+* Cross-version Python compatibility
+
+--------------------------------------------------------------------------------
+
+2012 by Daniel Foerster <pydsigner@gmail.com> and redistributable under the 
+LGPL, version 3 or later.
+
+--------------------------------------------------------------------------------
+
+This is the core connection manager.
+'''
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+class IRCConn:
+    
+    '''
+    This class handles the connection with the IRC server.
+    It connects and sends and receives messages.
+    '''
+    
+    def __init__(self, handler):
+        self.handler = handler
+        i = handler.ident
+        self.ident = i.ident
+        self.serv = i.serv
+        self.host = i.host
+        self.name = i.name
+        self.nick = i.nick
+        self.join_first = i.joins
+        self.port = 6667
+    
+    def connect(self):
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock.connect((self.host, self.port))
+        self._send('USER {} {} {} :{}'.format(self.ident, self.host, self.serv, self.name))
+        self._send('NICK {}'.format(self.nick))
+        # wait until we have received the MOTD in full before proceeding
+
+    def on_connect(self):
+        '''
+        Called once we have connected to and identified with the server.
+        Mainly joins the channels that we want to join at the start.
+        '''
+        for chan in self.join_first:
+            self.join(chan)
+    
+    def join(self, chan):
+        self._send('JOIN {}'.format(chan))
+    
+    def _send(self, msg):
+        '''
+        Send something (anything) to the IRC server.
+        '''
+        print('sending: {}\r\n'.format(msg))
+        self.sock.send('{}\r\n'.format(msg).encode())
+    
+    def say(self, msg, chan, to=None):
+        '''
+        Say @msg on @chan.
+        '''
+        prefix = '' if to is None else '{}: '.format(to)
+        for line in msg.splitlines():
+            self._send('PRIVMSG {} :{}{}'.format(chan, prefix, line))
+
+    def pong(self, trail):
+        self._send('PONG {}'.format(trail))
+    
+    def receive(self):
+        '''
+        Read from the socket until we reach the end of an IRC message.
+        Attempt to decode the message and return it.
+        Call handle_encoding_error() if unsuccessful.
+        '''
+        buf = []
+        while True:
+            nxt_ch = None
+            ch = self.sock.recv(1)
+            if ch == b'\r':
+                nxt_ch = self.sock.recv(1)
+                if nxt_ch == b'\n':
+                    try:
+                        line = b''.join(buf).decode()
+                    except (UnicodeEncodeError, UnicodeDecodeError):
+                        self.handle_encoding_error()
+                        return ''
+                    print('received: {}'.format(line)
+                    return line
+            buf.append(ch)
+            if nxt_ch:
+                buf.append(nxt_ch)
+            
+        if not line.strip():
+            return None
+        else:
+            try:
+                parsable = line.strip(b'\r\n').decode()
+                print('received: {}'.format(parsable))
+                return parsable
+            except (UnicodeEncodeError, UnicodeDecodeError):
+                self.handle_encoding_error()
+    
+    def parse(self, line):
+        if not line:
+            # empty line; this should throw up an error.
+            return
+        line = line.strip('\r\n')
+        tokens = line.split(' ')
+        if tokens[0].startswith(':'):
+            prefix = tokens.pop(0)[1:].strip(':')
+        else:
+            prefix = ''
+        
+        cmd = tokens.pop(0)
+        if cmd == '433':    # nick already in use
+            self.nick += '_'
+            self._send('NICK {}'.format(self.nick))
+        if cmd == '376':    # end of MOTD
+            self.on_connect()
+        if cmd == 'PING':
+            self.pong(' '.join(tokens))
+        elif cmd == 'ERROR':
+            self.handle_error(tokens)
+        elif cmd == 'JOIN':
+            if prefix.split('!')[0] != self.nick:
+                self.handler.handle_other_join(tokens, prefix)
+        elif cmd == 'PRIVMSG':
+            self.handler.handle_privmsg(tokens, prefix)
+    
+    def mainloop(self):
+        '''
+        The mainloop.
+        '''
+        while True:
+            line = self.receive()
+            self.parse(line)
+
+    def handle_encoding_error(self):
+        print('encoding error encountered.')
+    
+    def handle_error(self, tokens):
+        print('error. tokens: {}'.format(tokens)
+        self.connect()
