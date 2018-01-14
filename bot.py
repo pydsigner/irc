d0a91a3a2c9da272af96acd5a484a20b19bb2e38
Daniel Foerster <pydsigner@gmail.com>
2018-01-13 22:32:02 -0600

    Fix import and whitespace

--- bot.py
+++ bot.py
@@ -2,15 +2,15 @@
 This is a base for an IRC bot.
 """
 
-import connect
+from . import connect
 
 
 class Bot(object):
-    
+
     """
     An IRC bot interface.
     """
-    
+
     def __init__(self, com, ident):
         """
         @com and @ident must be classes that presents a particular interface.
@@ -20,7 +20,7 @@ class Bot(object):
         self.conn = connect.IRCConn(self)
         self.cmds = com(self)
         self.conn.connect()
-    
+
     def handle_privmsg(self, tokens, sender):
         chan = tokens.pop(0)
         tokens[0] = tokens[0].strip(':')
@@ -36,13 +36,13 @@ class Bot(object):
             chan = nick
         data = {'channel': chan, 'sender': sender, 'is_to_me': is_to_me,
                 'nick': nick, 'host': host}
-        
+
         cmd = tokens[0]
         try:
             args = tokens[1:]
         except IndexError:
             args = []
-        
+
         if is_to_me and (cmd in getattr(self.cmds, 'addr_funcs', {})):
             self.cmds.addr_funcs[cmd](args, data)
         elif cmd in getattr(self.cmds, 'unaddr_funcs', {}):
@@ -50,7 +50,7 @@ class Bot(object):
         else:
             for func in getattr(self.cmds, 'all_privmsg_funcs', []):
                 func(l + tokens, data)
-    
+
     def handle_name_list(self, tokens):
         tokens.pop(0)   # get rid of our nick from the beginning of the msg.
         tokens.pop(0)   # get rid of the equals sign(?!)
@@ -58,18 +58,18 @@ class Bot(object):
         nicks = [t.lstrip(':') for t in tokens]
         for func in getattr(self.cmds, 'on_name_list', []):
             func(nicks, channel)
-    
+
     def handle_join(self, channel):
         for func in getattr(self.cmds, 'on_join_funcs', []):
             func(channel)
-        
+
     def handle_other_join(self, tokens, joiner):
         chan = tokens.pop().strip(':')
         nick, host = joiner.split('!')
         data = {'channel': chan, 'joiner': joiner, 'nick': nick, 'host': host}
         for func in getattr(self.cmds, 'other_join_funcs', []):
             func(data)
-    
+
     def handle_kick(self, tokens, sender):
         chan = tokens.pop(0)
         tokens[0] = tokens[0].strip(':')
@@ -78,17 +78,17 @@ class Bot(object):
             is_about_me = True
         else:
             is_about_me = False
-        
+
         nick, host = sender.split('!')
         data = {'channel': chan, 'sender': sender, 'nick': nick, 'host': host, 'kickee': tokens.pop(0)}
         msg = tokens[0].lstrip(':') if tokens else None
-        if is_about_me: 
+        if is_about_me:
             for func in getattr(self.cmds, 'on_kicked_funcs', []):
                 func(msg, data)
         else:
             for func in getattr(self.cmds, 'other_kicked_funcs', []):
                 func(msg, data)
-    
+
     def main(self):
         """
         Run the mainloop.
@@ -96,22 +96,22 @@ class Bot(object):
         self.conn.mainloop()
 
 class BunBot(Bot):
-    
+
     """
-    An IRC bot interface compatible with bunburya's original bot. This Bot() 
+    An IRC bot interface compatible with bunburya's original bot. This Bot()
     supports command reloading, but is more stringent in its setup requirements.
     """
-    
+
     def __init__(self, confmod):
         """
-        @confmod must be a module with Identity() and CommandLib() classes. 
-        These classes must follow the same protocol as the @ident and @com 
+        @confmod must be a module with Identity() and CommandLib() classes.
+        These classes must follow the same protocol as the @ident and @com
         arguments to Bot().
         """
-        
+
         Bot.__init__(self, confmod.CommandLib, confmod.Identity)
         self.config = confmod
         self.main()
-    
+
     def reload_cmds(self):
         self.cmds = reload(self.config).CommandLib(self)
